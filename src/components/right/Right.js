92dbedaf582281815205bd78916a12e937422deb
Andy Humes <andrew.humes@du.edu>
2022-11-05 13:46:59 -0600

    merge

--- src/components/right/Right.js
+++ src/components/right/Right.js
@@ -8,15 +8,14 @@ function Right() {
     return (
       <div className="right">
       <div className='contact'>
-<Typography variant="h4" > Regional Director of Operations </Typography>
-<Typography variant="h6" > Denver, CO </Typography>
+
+<Typography variant="h6" > Connect </Typography>
 </div>
 <a href="https://www.linkedin.com/in/jackie-bayne-bs-coa-155b20174/" class="btn btn--gradient btn--xlrg" target="_blank" title="resume" rel="noreferrer"><span class="btn__text">Resume</span></a>
 <a href="mailto:jbayne@gmail.com" class="btn btn--gradient btn--xlrg" target="_blank" title="email" rel="noreferrer"><span class="btn__text">Email</span></a>
 <a href="https://www.linkedin.com/in/jackie-bayne-bs-coa-155b20174/" class="btn btn--gradient btn--xlrg" target="_blank" title="linkedin" rel="noreferrer"><span class="btn__text">LinkedIn</span></a>
 <a href="https://www.linkedin.com/in/jackie-bayne-bs-coa-155b20174/" class="btn btn--gradient btn--xlrg" target="_blank" title="facebook" rel="noreferrer"><span class="btn__text">Facebook</span></a>
-<p>Phone: 614-934-3435</p>
-humes.andre@gmail.com
+
      
       </div>
     );

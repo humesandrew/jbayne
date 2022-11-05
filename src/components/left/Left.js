556b0932e316114dc583229e01153930e41a54f7
Andy Humes <andrew.humes@du.edu>
2022-11-05 12:52:12 -0600

    addimage

--- src/components/left/Left.js
+++ src/components/left/Left.js
@@ -1,17 +1,28 @@
 
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+
 import resumeData from "../../resumeData.js";
 import './left.scss';
 
-function Left() {
-    return (
-      <div className="left">
-      
-<h2> Left
-</h2>
-<img alt="Andy Humes Portfolio" src={resumeData.image}></img>
 
-      </div>
-    );
-  }
 
-  export default Left
+export default function Left() {
+  return (
+    <Box sx={{ width: '100%' }} className="left">
+      <Stack spacing={2}>
+       <div className='imageDiv'><img src={resumeData.image} alt={resumeData.name}></img></div>
+       <div className='credentialsDiv'>Credentials</div>
+       <div className='contactDiv'>Contact</div>
+      </Stack>
+    </Box>
+  );
+}
+
+
+
